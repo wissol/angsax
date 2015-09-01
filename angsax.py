440593df6fc01636c72aa291e19d0cdbad7d7602
Miguel de Luis <miguel@sabiavida.com>
2015-09-01 16:58:50 +0000

    refactorizacion limpiar code

--- angsax.py
+++ angsax.py
@@ -4,7 +4,7 @@ from narrativa import secciones
 
 app = Flask(__name__)
 
-def imagen_aleatoria(y):
+def annade_imagen_aleatoria(y):
     ''' elige una imagen aleatoria de una lista si no se ha establecido ninguna
     '''
     if (not y['imagen-arriba'] ) and (not y['imagen-abajo']):
@@ -47,7 +47,9 @@ def inicio():
 
 @app.route('/', methods=['POST'])
 def section():
-    opcion_elegida = request.form['opciones']
-    y = siguiente_seccion(secciones, opcion_elegida)
-    y = imagen_aleatoria(y)
-    return render_template('index.html', y = y)
+    return render_template(
+        'index.html',
+        y = annade_imagen_aleatoria(
+            siguiente_seccion(secciones, request.form['opciones'])
+        )
+    )
