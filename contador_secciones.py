eb3b07f2f8561e7d130d256fb04751f8df3707e8
Miguel de Luis <miguel@sabiavida.com>
2015-09-16 17:10:56 +0000

    contador secciones cuenta los finales

--- contador_secciones.py
+++ contador_secciones.py
@@ -1,10 +1,16 @@
 from narrativa import secciones
 
 pruebas = 0
+fines = 0
 for seccion in secciones:
     if seccion[0] == 'p':
         pruebas += 1
+    else:
+        x = secciones[seccion]
+        if x['opciones'] == False:
+            fines += 1
 
 print("secciones: ", len(secciones))
 print("pruebas: ",pruebas)
 print("visibles: ", len(secciones) - pruebas)
+print("finales: ", fines)
